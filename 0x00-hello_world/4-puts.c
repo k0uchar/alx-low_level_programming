9c5289b2c9c28bb078534b2f504f12e9c6cd30a0
k0uchar <kouchar.mohammed@outlook.com>
2023-09-07 11:52:04 -0700

    C Tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+/**
+ * main - main function
+ * Return: 0 (success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+/**
+ * main - main function
+ * Return: 0 (success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\\n");
+    return (0);
+}
